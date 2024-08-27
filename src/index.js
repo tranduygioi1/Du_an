0e8e011586965dc101f31a47e4cbdc6df9cc26dd
tranduygioi1 <tranduygioi1108@gmail.com>
2024-08-27 16:04:31 +0700

    change some texts

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ app.engine(
     extname: '.hbs',
   }),
 );
-                    app.set('view engine', 'hbs');
+app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
 // Routers init
