e1fc40095283a3df66f542bb5a0de7730c94af72
tranduygioi1 <tranduygioi1108@gmail.com>
2024-08-27 15:17:03 +0700

    Changer some texts

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ const { engine } = require('express-handlebars');
 var app = express();
 const POST = 4400;
 
-                                                const route = require('./routes');
+const route = require('./routes');
 
-                                                app.use(express.static(path.join(__dirname, 'public')));
-                                                app.use(
-                                                express.urlencoded({
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  express.urlencoded({
     extended: true,
   }),
 );
@@ -19,18 +19,18 @@ app.use(express.json());
 // app.use(morgan('combined'))
 
 //temolate engine
-                                                                                                app.engine(
-                                                                                                'hbs',
-                                                                                                engine({
-                                                                                                    extname: '.hbs',
-                                                                                                }),
-                                                                                                );
-                                                                                                app.set('view engine', 'hbs');
-                                                                                                app.set('views', path.join(__dirname, 'resources/views'));
+app.engine(
+  'hbs',
+  engine({
+    extname: '.hbs',
+  }),
+);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
 
-                                                                                                // Routers init
-                                                                                                route(app);
+// Routers init
+route(app);
 
-                                                                                                app.listen(POST, () => {
+app.listen(POST, () => {
   console.log('Start Server');
 });
